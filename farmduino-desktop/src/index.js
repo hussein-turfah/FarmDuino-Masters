51695ad5dc55d573b1192a3887409fe5a5eebcdd
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-22 01:38:55 +0200

    :wrench: Fix for vercel dep

--- farmduino-desktop/src/index.js
+++ farmduino-desktop/src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; // Correct import
+
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <BrowserRouter basename="/">
-//     <App />
-//   </BrowserRouter>
-// );
 
 ReactDOM.render(
   <BrowserRouter basename="/">
