4c0bb76850b1590da84411d0eb968d54a0d12c0b
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-22 02:29:20 +0200

    :wrench: Fix for vercel dep

--- farmduino-desktop/src/hooks/http-request.js
+++ farmduino-desktop/src/hooks/http-request.js
@@ -1,6 +1,6 @@
 const UseHttp = async (url, method = "GET", body = "", header = {}) => {
   let data;
-  const URL = "http://localhost:8000/api/v1.0.0/";
+  const URL = "https://farmduino-server.vercel.app//api/v1.0.0/";
   try {
     const Response =
       (await fetch(URL + url, {
